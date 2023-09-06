de1279b38cd52afd4e35117f55826e2299f3ae45
_TatarinBarin_ <issushristos@gmail.com>
2023-09-06 18:23:36 +0300

    HW6

---
+++ hw(41)/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше "0" ввёл пользователь.
+
+// 0, 7, 8, -2, -4 -> 2
+
+Console.Write("Какое кол-во чисел мы будем вводить? -> ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] SaveEnterNumbers(int sizeArr)
+{
+    int[] EnterNumbers = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите число № {i + 1} -> ");
+        EnterNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return EnterNumbers;
+}
+
+int SumEnterPositiveNumbers(int[] arr)
+{
+    int index = 0;
+    int sum = 0;
+    while (index < size)
+    {
+        if (arr[index] > 0) sum += 1;
+        index++;
+    }
+    return sum;
+}
+
+int[] arr = SaveEnterNumbers(size);
+int sumEnterPositiveNumbers = SumEnterPositiveNumbers(arr);
+
+void PrintEnterNumbers(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write($" -> {sumEnterPositiveNumbers}");
+}
+
+PrintEnterNumbers(arr);
---
+++ hw(43)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2, k2 вводит пользователь.
+
+
+void PointIntersectionTwoStraight(double b1, double k1, double b2, double k2)
+{
+
+    double x = (-b2 + b1) / (-k1 + k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"Точка пересечения двух прямых X: {x}, Y: {y}");
+
+}
+
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+PointIntersectionTwoStraight(k1, b1, k2, b2);
